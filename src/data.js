fbe38bd0e80e41b22620d41eaf5ed795489579d3
Kabogo Michael <kabogomichael@kabogos-mbp.lan>
2024-09-24 11:46:45 +0300

    I updated a data.js filee

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import  greekSalad from './components/icons_assets/greek salad.jpg'
+import Bruchetta from './components/icons_assets/bruchetta.svg'
+import Lemon from './components/icons_assets/lemon dessert.jpg'
+export const data = [
+    {
+        id:1,
+        image:greekSalad,
+        title:'Greek Salad',
+        price : 12.99,
+        description:"The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. "
+
+    },
+    {
+        id:2,
+        image:Bruchetta,
+        title:'Bruchetta',
+        price : 7.99,
+        description:"Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. Toppings of tomato, veggies, beans, cured pork, or cheese are examples of variations. In Italy, a brustolina grill is frequently used to create bruschetta. "
+
+    },
+    {
+        id:3,
+        image:Lemon,
+        title:'Lemon Dessert',
+        price : 6.99,
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla odio enim vitae. "
+
+    },
+
+]
